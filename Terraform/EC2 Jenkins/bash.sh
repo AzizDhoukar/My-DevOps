32804fb8cf4f25e6fbf800576ad9946b34b0322b
Aziz Dhoukar <aziz.dhoukar@gmail.com>
2024-05-26 19:26:28 +0100

    writing a tf file for jenkins

---
+++ Terraform/EC2 Jenkins/bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo amazon-linux-extras install java-17-amazon-corretto -y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins -y
+
+sudo systemctl start jenkins
--- Terraform/EC2/main.tf
+++ Terraform/EC2/main.tf
@@ -43,7 +43,4 @@ resource "aws_eip" "eip" {
   domain         = "vpc"
 }
 
-# Output the EC2 instance's public IP
-output "instance_public_ip" {
-  value = aws_eip.eip.public_ip
-}
+
