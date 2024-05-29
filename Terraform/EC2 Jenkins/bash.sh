fbb22f4e3b425d84fc64331ce22725670ce37230
Aziz Dhoukar <aziz.dhoukar@gmail.com>
2024-05-29 18:46:21 +0100

    making terraform for kubernetes

--- Terraform/EC2 Jenkins/bash.sh
+++ Terraform/EC2 Jenkins/bash.sh
@@ -4,4 +4,4 @@ sudo amazon-linux-extras install docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 #docker pull jenkins/jenkins
-#docker run -p 8080:8080 -p 50000:50000 -v /your/home:/var/jenkins_home jenkins/jenkins
+#docker run -p 8080:8080 -p 50000:50000 -v home:/var/jenkins_home jenkins/jenkins
