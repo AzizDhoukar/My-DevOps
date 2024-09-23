bc9e5dd6ed9a13da4238f2b4c6b42feef6f74480
Aziz Dhoukar <aziz.dhoukar@gmail.com>
2024-09-23 22:18:03 +0100

    writing the sh for ansibl installation

--- Jenkins-node-automation/jenkins_node.sh
+++ Jenkins-node-automation/jenkins_node.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sudo apt update
-sudo apt install git openjdk-17-jre -y
-sudo useradd -d /var/lib/jenkins jenkins
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
 
+#sudo apt update
+#sudo apt install git openjdk-17-jre -y
+#sudo useradd -d /var/lib/jenkins jenkins
