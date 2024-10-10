fb84e79ea289090dd02db2d659db621a78b8c8eb
Aziz Dhoukar <aziz.dhoukar@gmail.com>
2024-10-10 22:07:22 +0100

    made progress on the k8s deployment

--- k8s-deployment-automation/terraform/inventory.tf
+++ k8s-deployment-automation/terraform/inventory.tf
@@ -4,12 +4,12 @@ resource "local_file" "inventory" {
   content = <<EOF
 [all:vars]
 ansible_user=ubuntu
-ansible_ssh_private_key_file=~/.ssh/${aws_key_pair.k8s-key.key_name}.pem
+ansible_ssh_private_key_file=~/.ssh/${aws_key_pair.k8s-key.key_name}
 
 [k8s-master]
 master ansible_host=${aws_instance.k8s-master.public_ip}
 
-[k8s-worker]
+[k8s-workers]
 node ansible_host=${aws_instance.k8s-worker.public_ip}
 EOF
 }
